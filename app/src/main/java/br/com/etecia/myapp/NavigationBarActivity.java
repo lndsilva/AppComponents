12ba8c46f044aee0eefe2e5399a397611af5613b
lndsilva <lndsilva@hotmail.com>
2023-11-10 17:08:29 -0300

    aplicando itens nas janelas - fragments

--- app/src/main/java/br/com/etecia/myapp/NavigationBarActivity.java
+++ app/src/main/java/br/com/etecia/myapp/NavigationBarActivity.java
@@ -15,6 +15,10 @@ public class NavigationBarActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
 
     ExploreFragment exploreFragment = new ExploreFragment();
+    ContributeFragment contributeFragment = new ContributeFragment();
+    SavedFragment savedFragment = new SavedFragment();
+    GoFragment goFragment = new GoFragment();
+    UpdatesFragment updatesFragment = new UpdatesFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +32,20 @@ public class NavigationBarActivity extends AppCompatActivity {
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if (item.getItemId() == R.id.idRVExplore) {
+                if (item.getItemId() == R.id.mExplore) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.idContainerNavView, exploreFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.mGo) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.idContainerNavView, goFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.mSaved) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.idContainerNavView, savedFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.mContribute) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.idContainerNavView, contributeFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.mUpdates) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.idContainerNavView, updatesFragment).commit();
                     return true;
                 }
 
